4a142bebbfd36e23eb38e5e852e74b6b81c6bee2
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/DXPivotGrid_FormattingViaEvents/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows;
+using DevExpress.Xpf.PivotGrid;
+using FormattingViaEvents.DataSet1TableAdapters;
+
+namespace FormattingViaEvents {
+    public partial class MainWindow : Window {
+        FormattingViaEvents.DataSet1.SalesPersonDataTable salesPersonDataTable =
+            new FormattingViaEvents.DataSet1.SalesPersonDataTable();
+        SalesPersonTableAdapter salesPersonDataAdapter = new SalesPersonTableAdapter();
+        public MainWindow() {
+            InitializeComponent();
+            pivotGridControl1.DataSource = salesPersonDataTable;
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e) {
+            salesPersonDataAdapter.Fill(salesPersonDataTable);
+        }
+        private void pivotGridControl1_CustomCellDisplayText(object sender,
+            PivotCellDisplayTextEventArgs e) {
+                if (e.RowValueType != FieldValueType.GrandTotal ||
+                    e.ColumnValueType == FieldValueType.GrandTotal ||
+                    e.ColumnValueType == FieldValueType.Total) return;
+                if (Convert.ToSingle(e.Value) < 50000)
+                    e.DisplayText = "Low";
+                else if (Convert.ToSingle(e.Value) > 100000)
+                    e.DisplayText = "High";
+                else
+                    e.DisplayText = "Middle";
+        }
+    }
+}
---
+++ VB/DXPivotGrid_FormattingViaEvents_VB/MainWindow.xaml.vb
@@ -0,0 +1,40 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Data
+Imports System.Data.OleDb
+Imports System.Windows
+Imports DevExpress.Xpf.PivotGrid
+Imports DevExpress.XtraPivotGrid
+Imports DXPivotGrid_FormattingViaEvents_VB.nwindDataSetTableAdapters
+
+Namespace DXPivotGrid_FormattingViaEvents_VB
+    Partial Public Class MainWindow
+        Inherits Window
+        Private salesPersonDataTable As New nwindDataSet.SalesPersonDataTable()
+        Private salesPersonDataAdapter As New SalesPersonTableAdapter()
+        Public Sub New()
+            InitializeComponent()
+            pivotGridControl1.DataSource = salesPersonDataTable
+            pivotGridControl1.PrefilterString = "[Country] == 'UK' OR [Country] == 'USA'"
+
+        End Sub
+        Protected Sub pivotGridControl1_CustomCellDisplayText(ByVal sender As Object, _
+                                                    ByVal e As PivotCellDisplayTextEventArgs)
+            If e.RowValueType <> PivotGridValueType.GrandTotal OrElse
+                e.ColumnValueType = PivotGridValueType.GrandTotal OrElse
+                e.ColumnValueType = PivotGridValueType.Total Then
+                Return
+            End If
+            If Convert.ToSingle(e.Value) < 50000 Then
+                e.DisplayText = "Low"
+            ElseIf Convert.ToSingle(e.Value) > 100000 Then
+                e.DisplayText = "High"
+            Else
+                e.DisplayText = "Middle"
+            End If
+        End Sub
+        Private Sub Window_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            salesPersonDataAdapter.Fill(salesPersonDataTable)
+        End Sub
+    End Class
+End Namespace
+
